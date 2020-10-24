280e73e46150097f15f8058a5807695c46076401
Joel Welling <welling@psc.edu>
2020-10-23 23:26:57 -0400

    Fix missing import, add OME.TIFF extension support.

--- src/ingest_validation_tests/ome_tiff_validator.py
+++ src/ingest_validation_tests/ome_tiff_validator.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import xmltodict
 import tifffile
 from ingest_validation_tools.plugin_validator import Validator
@@ -7,12 +9,13 @@ class OmeTiffValidator(Validator):
     cost = 1.0
     def collect_errors(self) -> List[str]:
         rslt = []
-        for path in self.path.glob('**/*.ome.tiff') + self.path.glob('**/*.OME.TIFF'):
-            try:
-                with tifffile.TiffFile(path) as tf:
-                    metadata = xmltodict.parse(tf.ome_metadata)  # @Notused
-            except:
-                rslt.append(f'{path} is not a valid PNG file')
-                raise
+        for glob_expr in ['**/*.ome.tiff', '**/*.OME.TIFF']:
+            for path in self.path.glob(glob_expr):
+                try:
+                    with tifffile.TiffFile(path) as tf:
+                        metadata = xmltodict.parse(tf.ome_metadata)  # @Notused
+                except:
+                    rslt.append(f'{path} is not a valid OME.TIFF file')
         return rslt
+
 
