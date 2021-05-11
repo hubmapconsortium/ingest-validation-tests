039145e82b30ad9ff034979f647b2a637323fc15
Joel Welling <welling@psc.edu>
2021-05-11 13:48:50 -0400

    Validate the XML content of ome-tiffs against its schema

--- src/ingest_validation_tests/ome_tiff_validator.py
+++ src/ingest_validation_tests/ome_tiff_validator.py
@@ -1,6 +1,6 @@
 from typing import List
 
-import xmltodict
+import xmlschema
 import tifffile
 from ingest_validation_tools.plugin_validator import Validator
 
@@ -13,7 +13,9 @@ class OmeTiffValidator(Validator):
             for path in self.path.glob(glob_expr):
                 try:
                     with tifffile.TiffFile(path) as tf:
-                        metadata = xmltodict.parse(tf.ome_metadata)  # @Notused
+                        xml_document = xmlschema.XmlDocument(tf.ome_metadata)
+                    if not xml_document.schema.is_valid(xml_document):
+                        rslt.append(f'{path} is not a valid OME.TIFF file')
                 except:
                     rslt.append(f'{path} is not a valid OME.TIFF file')
         return rslt
