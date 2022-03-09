86639a62d16e971f6246501bc843de8a644085ac
mccalluc <mccalluc@users.noreply.github.com>
2022-03-09 15:52:29 -0500

    CODEX schema check: No unit testing, so not sure if it works

---
+++ src/ingest_validation_tests/codex_json_validator.py
@@ -0,0 +1,26 @@
+from typing import List
+from pathlib import Path
+import json
+
+from jsonschema import validate
+
+from ingest_validation_tools.plugin_validator import Validator
+
+
+class CodexJsonValidator(Validator):
+    description = "Check CODEX JSON against schema"
+    cost = 1.0
+
+    def collect_errors(self, **kwargs) -> List[str]:
+        schema_path = Path(__file__).parent / 'codex_schema.json'
+        schema = json.loads(schema_path.read_text)
+
+        rslt = []
+        for glob_expr in ['**/[Ee]xperiment.json']:
+            for path in self.path.glob(glob_expr):
+                instance = json.loads(path.read_text())
+                try:
+                    validate(instance=instance, schema=schema)
+                except Exception as e:
+                    rslt.append(f'{path}: {e}')
+        return rslt
