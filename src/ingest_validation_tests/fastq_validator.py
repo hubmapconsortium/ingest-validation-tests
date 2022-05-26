7c5c768d23d6116f090a3c547c60dfaf4295e535
Marc Chametzky <marc@bluevine.net>
2022-05-26 15:52:16 -0400

    Added initial FASTQValidator tests. (checkpoint)

--- src/ingest_validation_tests/fastq_validator.py
+++ src/ingest_validation_tests/fastq_validator.py
@@ -1,5 +1,34 @@
-"""Evaluate an optionally-compressed FASTQ file for basic syntax."""
+from pathlib import Path
+from typing import List, Generator
+
+from ingest_validation_tools.plugin_validator import Validator
+
+_FASTQ_FILE_MATCH = '**/*.fastq*'
 
 
 def has_valid_name(filename: str) -> bool:
     return filename.endswith('.fastq.gz') or filename.endswith('.fastq')
+
+
+class FASTQValidator(Validator):
+    """Validate FASTQ input files for basic syntax."""
+
+    def collect_errors(self, **kwargs) -> List[str]:
+        self.errors: List[str] = []
+        found_one = False
+
+        fastq_file: Path
+        for fastq_file in self.path.glob(_FASTQ_FILE_MATCH):
+            found_one = True
+
+            if not has_valid_name(fastq_file.name):
+                self.errors.append(
+                    f"{fastq_file} is not an expected filename and will "
+                    "not be processed."
+                )
+
+        if not found_one:
+            self.errors.append(f"No files matching {_FASTQ_FILE_MATCH} were "
+                               f"found in {self.path}.")
+
+        return self.errors
--- tests/test_fastq_validator.py
+++ tests/test_fastq_validator.py
@@ -1,12 +1,17 @@
+import gzip
 import os
+from pathlib import Path
+import pytest
 import sys
+from typing import TextIO
 
 # Ensure access to submodules' Python module to gain full access to imports'
 # dependencies.
 
 sys.path.insert(0, os.path.abspath('../../..'))
 from submodules import ingest_validation_tests
-from ingest_validation_tests.fastq_validator import has_valid_name
+from ingest_validation_tests.fastq_validator import has_valid_name, \
+    FASTQValidator
 
 
 class TestHasValidName:
@@ -16,5 +21,42 @@ class TestHasValidName:
     def test_has_valid_name_fastq(self):
         assert has_valid_name('test.fastq')
 
-    def test_has_valid_name_invalid_gz(self):
+    def test_has_valid_name_invalid_gz_filename(self):
         assert not has_valid_name('test.gz')
+
+
+_ASSAY_TYPE = 'snRNAseq'
+
+# 1000 below increments for subsequent records
+_GOOD_RECORDS = '''\
+@A12345:123:A12BCDEFG:1:1234:1000:1234 1:N:0:NACTGACTGA+CTGACTGACT
+NACTGACTGA
++
+#FFFFFFFFF
+'''
+
+
+class TestFASTQValidator:
+    @pytest.fixture
+    def fastq_validator(self, tmp_path) -> FASTQValidator:
+        return FASTQValidator(tmp_path, _ASSAY_TYPE)
+
+    def test_fastq_validator_no_files(self, fastq_validator):
+        result = fastq_validator.collect_errors()
+        assert "No files matching" in result[0]
+
+    def _open_output_file(self, filename: Path, use_gzip: bool) -> TextIO:
+        return (
+            gzip.open(filename, 'wt') if use_gzip
+            else open(filename, 'wt')
+        )
+
+    @pytest.mark.parametrize("use_gzip", [False, True])
+    def test_fastq_validator_good_file(self, fastq_validator, tmp_path,
+                                       use_gzip):
+        test_file = tmp_path.joinpath('test.fastq')
+        with self._open_output_file(test_file, False) as output:
+            output.write(_GOOD_RECORDS)
+
+        result = fastq_validator.collect_errors()
+        assert not result
