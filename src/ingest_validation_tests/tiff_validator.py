4bff34dcf4cf78e7214ddc2886b85782c833aa3e
Joel Welling <welling@psc.edu>
2023-05-25 15:37:49 -0400

    partial pylint

--- src/ingest_validation_tests/tiff_validator.py
+++ src/ingest_validation_tests/tiff_validator.py
@@ -1,3 +1,4 @@
+from os import cpu_count
 from multiprocessing import Pool
 from typing import List
 
@@ -8,24 +9,24 @@ def _log(message: str):
     print(message)
 
 
-def _check_tiff_file(path:str) -> str or None:
+def _check_tiff_file(path: str) -> str or None:
     try:
-        with tifffile.TiffFile(path) as tf:
-            for page in tf.pages:
+        with tifffile.TiffFile(path) as tfile:
+            for page in tfile.pages:
                 _ = page.shape
                 _ = page.dtype
             return None
     except Exception as excp:
         _log(f"{path} is not a valid TIFF file: {excp}")
-        return(f"{path} is not a valid TIFF file")
-            
+        return f"{path} is not a valid TIFF file"
+
 
 class TiffValidator(Validator):
     description = "Recursively test all tiff files that are not ome.tiffs for validity"
     cost = 1.0
     def collect_errors(self, **kwargs) -> List[str]:
-        threads = kwargs.get('coreuse', None) or os.cpu_count() // 4 or 1
-        print(f"THREADS: {threads}")
+        threads = kwargs.get('coreuse', None) or cpu_count() // 4 or 1
+        print(f"THREADS {threads}")
         pool = Pool(threads)
         filenames_to_test = []
         for glob_expr in ['**/*.tif', '**/*.tiff', '**/*.TIFF', '**/*.TIF']:
