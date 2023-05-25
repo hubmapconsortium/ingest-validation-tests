c440aac477057e0724cb87053fb55e6ce5574e71
Joel Welling <welling@psc.edu>
2023-05-25 15:17:09 -0400

    WIP

--- src/ingest_validation_tests/tiff_validator.py
+++ src/ingest_validation_tests/tiff_validator.py
@@ -1,23 +1,37 @@
+from multiprocessing import Pool
 from typing import List
 
 import tifffile
 from ingest_validation_tools.plugin_validator import Validator
 
+def _log(message: str):
+    print(message)
+
+
+def _check_tiff_file(path:str) -> str or None:
+    try:
+        with tifffile.TiffFile(path) as tf:
+            for page in tf.pages:
+                _ = page.shape
+                _ = page.dtype
+            return None
+    except Exception as excp:
+        _log(f"{path} is not a valid TIFF file: {excp}")
+        return(f"{path} is not a valid TIFF file")
+            
+
 class TiffValidator(Validator):
     description = "Recursively test all tiff files that are not ome.tiffs for validity"
     cost = 1.0
     def collect_errors(self, **kwargs) -> List[str]:
-        rslt = []
+        threads = kwargs.get('coreuse', None) or os.cpu_count() // 4 or 1
+        print(f"THREADS: {threads}")
+        pool = Pool(threads)
+        filenames_to_test = []
         for glob_expr in ['**/*.tif', '**/*.tiff', '**/*.TIFF', '**/*.TIF']:
             for path in self.path.glob(glob_expr):
-                # skip ome.tiffs; they are checked elsewhere
-                if path.name.lower().endswith(('.ome.tif', '.ome.tiff')):
-                    continue
-                try:
-                    with tifffile.TiffFile(path) as tf:
-                        for page in tf.pages:
-                            _ = page.shape
-                            _ = page.dtype
-                except:
-                    rslt.append(f'{path} is not a valid TIFF file')
-        return rslt
+                if not path.name.lower().endswith(('.ome.tif', '.ome.tiff')):
+                    filenames_to_test.append(path)
+        return list(rslt for rslt in pool.imap_unordered(_check_tiff_file,
+                                                         filenames_to_test)
+                    if rslt is not None)
