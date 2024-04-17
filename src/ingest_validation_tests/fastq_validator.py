cefd4e4f03ee342e9f13bc4b0a4173fb5f333d71
Joel Welling <welling@psc.edu>
2024-04-17 11:12:13 -0400

    Added version strings

--- src/ingest_validation_tests/fastq_validator.py
+++ src/ingest_validation_tests/fastq_validator.py
@@ -8,6 +8,7 @@ from ingest_validation_tools.plugin_validator import Validator
 class FASTQValidator(Validator):
     description = "Check FASTQ files for basic syntax and consistency."
     cost = 15.0
+    version = "1.0.0"
 
     def collect_errors(self, **kwargs) -> List[str]:
         threads = kwargs.get("coreuse", None) or cpu_count() // 4 or 1
