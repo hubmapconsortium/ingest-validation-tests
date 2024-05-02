a5427523fc949f80ec089f90c79d2f721562f20a
Joel Welling <welling@psc.edu>
2024-05-02 14:53:19 -0400

    Apply black

--- tests/test_semantic_versions.py
+++ tests/test_semantic_versions.py
@@ -9,14 +9,15 @@ FALLBACK_REGEX = r"^\d+\.\d+$"
 
 
 def test_plugin_semantic_versions():
-    plugin_dir = (Path(__file__).parent.parent
-                  / "src" / "ingest_validation_tests")
+    plugin_dir = Path(__file__).parent.parent / "src" / "ingest_validation_tests"
     fb_regex = re.compile(FALLBACK_REGEX)
     for cls in validation_class_iter(plugin_dir):
         try:
             _ = semantic_version.Version(cls.version)
         except ValueError:
             if not fb_regex.match(cls.version):
-                assert False, (f"The plugin {cls.__name__} has version string"
-                               f" {cls.version}, which is not a valid semantic"
-                               " version")
+                assert False, (
+                    f"The plugin {cls.__name__} has version string"
+                    f" {cls.version}, which is not a valid semantic"
+                    " version"
+                )
