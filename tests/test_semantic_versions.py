6d38f085d530955798a953226b24bcf28cab6b15
Joel Welling <welling@psc.edu>
2024-05-02 14:27:20 -0400

    More changes to please flake8

--- tests/test_semantic_versions.py
+++ tests/test_semantic_versions.py
@@ -1,23 +1,21 @@
 import re
-import zipfile
 from pathlib import Path
 
-import pytest
+import pytest  #noqa: F401
 import semantic_version
 
 from ingest_validation_tools.plugin_validator import validation_class_iter
 
 FALLBACK_REGEX = r"^\d+\.\d+$"
 
+
 def test_plugin_semantic_versions():
     plugin_dir = (Path(__file__).parent.parent
                   / "src" / "ingest_validation_tests")
-    classnames = []
     fb_regex = re.compile(FALLBACK_REGEX)
     for cls in validation_class_iter(plugin_dir):
-        validator = cls(['.'], 'someassay', plugin_dir, [])
         try:
-            ver = semantic_version.Version(cls.version)
+            _ = semantic_version.Version(cls.version)
         except ValueError:
             if not fb_regex.match(cls.version):
                 assert False, (f"The plugin {cls.__name__} has version string"
