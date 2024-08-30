938d0df3cdd85806bc9d3e017bb322ef24743a9e
David Betancur <dbetancur@psc.edu>
2024-08-30 14:25:24 -0400

    Updating to apply suggestions.

--- src/ingest_validation_tests/segmentation_mask_imagesize_validation.py
+++ src/ingest_validation_tests/segmentation_mask_imagesize_validation.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Union
 
 import tifffile
 import xmlschema
@@ -7,7 +7,7 @@ from ingest_validation_tools.plugin_validator import Validator
 from utils import GetParentData
 
 
-def get_ometiff_size(file) -> Optional[str, dict]:
+def get_ometiff_size(file) -> Union[str, dict]:
     try:
         tf = tifffile.TiffFile(file)
         xml_document = xmlschema.XmlDocument(tf.ome_metadata)
@@ -35,6 +35,12 @@ class ImageSizeValidator(Validator):
     cost = 1.0
     version = "1.0"
     required = "segmentation_mask"
+    files_to_find = [
+                    "**/*.ome.tif",
+                    "**/*.ome.tiff",
+                    "**/*.OME.TIFF",
+                    "**/*.OME.TIF",
+                ]
 
     def collect_errors(self, **kwargs) -> List[Optional[str]]:
         del kwargs
@@ -50,29 +56,20 @@ class ImageSizeValidator(Validator):
                 if not data_path.is_absolute():
                     data_path = Path(self.paths[0]).parent / data_path
 
-                for glob_expr in [
-                    "**/*.ome.tif",
-                    "**/*.ome.tiff",
-                    "**/*.OME.TIFF",
-                    "**/*.OME.TIF",
-                ]:
+                for glob_expr in self.files_to_find:
                     for file in data_path.glob(glob_expr):
                         filenames_to_test.append(file)
-                assert len(filenames_to_test) != 1, "Too many or too few files Mask"
 
-                for glob_expr in [
-                    "**/*.ome.tif",
-                    "**/*.ome.tiff",
-                    "**/*.OME.TIFF",
-                    "**/*.OME.TIF",
-                ]:
                     for file in Path(
                         GetParentData(
                             row["parent_dataset_id"], self.globus_token, self.app_context
                         ).get_path()
                     ).glob(glob_expr):
                         parent_filenames_to_test.append(file)
+
+                assert len(filenames_to_test) != 1, "Too many or too few files Mask"
                 assert len(parent_filenames_to_test) != 1, "Too many or too few files Base Images"
+
                 segmentation_mask_size = get_ometiff_size(filenames_to_test[0])
                 base_image_size = get_ometiff_size(parent_filenames_to_test[0])
                 assert (
