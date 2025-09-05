e65b4dd883298febb097744d025a941f3cbddee9
Gesina Phillips <gesina@psc.edu>
2025-09-05 13:15:35 -0400

    making it easier to run linting/formatting locally while testing to avoid unnecessary failing commits; skippable

--- test.sh
+++ test.sh
@@ -1,11 +1,78 @@
 #!/usr/bin/env bash
-# Accepts flags as individual args as well as custom "--test=" arg
-# which can be used to run a specific test file/class/test just as you
-# would pass it to pytest directly.
+# Example usage:
+# Run specific test in interactive debug mode (will not proceed if linting/formatting errors):
+#   ./test.sh -t tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_good -d
+# Same as above, skipping linting/formatting:
+#   ./test.sh -n -t tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_good -d
+# Run all tests, skip linting/formatting, no interactive debugger:
+#   ./test.sh -n
 
-# Example:
-# ./test.sh "--test=tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_good" --pdb
-#
-set -o errexit
+usage() { echo "Usage: $0 [-n] [-t <test_string>]
+    -n : skip linting/formatting
+    -t : run specific test; use pytest format
+        example: tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_bad
+    -d : pass --pdb to pytest for debugging
+    -- pass arbitrary other args following ' -- '" 1>&2; exit 1; }
 
-python tests/pytest_runner.py "$@"
+# Define expected options
+while getopts "nt:d" opt; do
+	case "$opt" in
+		n)
+			echo "Skipping linting/formatting"
+            SKIP="$1"
+		;;
+		t)
+            if [[ "$OPTARG" = "--" ]]; then
+                echo "-t must be followed by reference to a test"
+                usage
+            else
+                TEST="$OPTARG"
+            fi
+        ;;
+		d)
+            DEBUG="--pdb"
+		;;
+        ?)
+            usage
+        ;;
+	esac
+done
+shift $((OPTIND - 1))
+
+# Define -- as delimiter between options and arbitrary args to pass to pytest
+[[ $1 = "--" ]] && shift
+PARAMS=("$PARAMS$@")
+
+# Run linting/formatting if not skipped with -n
+# black/isort will auto-format
+if [ -z "$SKIP" ]; then
+    echo "Running linting/formatting checks..."
+    echo "--------"
+    echo "flake8"
+    flake8 --ignore=E501,W503,E203 .
+    if [ $? -ne 0 ]; then
+        ERROR=1
+    fi
+    echo "--------"
+    echo "black"
+    black --line-length 99 src
+    if [ $? -ne 0 ]; then
+        ERROR=1
+    fi
+    echo "--------"
+    echo "isort"
+    isort --profile black src
+    if [ $? -ne 0 ]; then
+        ERROR=1
+    fi
+fi
+
+# Exit if linting/formatting errors found
+if [[ $ERROR == 1 ]]; then
+    echo "Fix linting/formatting errors or pass -n/-no_lint to run tests."
+    exit 1
+fi
+
+# Run test suite with appropriate args
+echo "running python/tests_pytest_runner.py $TEST $DEBUG ${PARAMS[@]}"
+python tests/pytest_runner.py "$TEST" "$DEBUG" "${PARAMS[@]}" || exit 1
--- tests/pytest_runner.py
+++ tests/pytest_runner.py
@@ -25,18 +25,15 @@ class add_path:
 
 def main():
     """
-    Receives args from test.sh but can also accept pytest flags or
-    custom --test= arg directly, e.g.
-    python tests/pytest_runner.py --test="tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_good" --pdb
+    Receives args from test.sh but could also be invoked directly
+    python tests/pytest_runner.py tests/test_fastq_validator_logic.py::TestFASTQValidatorLogic::test_fastq_groups_good --pdb
     """
     plugins_path = Path(__file__).resolve().parent.parent / "src" / "ingest_validation_tests"
     with add_path(str(plugins_path)):
-        args = ["-vv"]
-        for arg in sys.argv[2:]:
-            if arg.startswith("--test="):
-                args.append(arg.replace("--test=", ""))
-            else:
-                args.append(arg)
+        args = []
+        for arg in sys.argv[1:]:
+            args.append(arg)
+        args.append("-vv")
         sys.exit(pytest.main(args))
 
 
